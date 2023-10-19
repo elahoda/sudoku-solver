c1c119b3e2d2d8ddd1c97e00bf23dcc75fd584a5
Eric Lahoda <elahoda@gmail.com>
2023-10-18 23:38:43 -0400

    greyed out and solvable

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ for (let i = 0; i < squares; i++) {
   inputElement.setAttribute("type", "number");
   inputElement.setAttribute("min", "1");
   inputElement.setAttribute("max", "9");
+  if (
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
+    ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && i > 27 && i < 53) ||
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
+  ) {
+    inputElement.classList.add("odd-section");
+  }
+
   puzzleBoard.appendChild(inputElement);
 }
 
@@ -30,6 +40,9 @@ const populateValues = (isSolvable, solution) => {
     inputs.forEach((input, i) => {
       input.value = solution[i];
     });
+    solutionDisplay.innerHTML = "You have the answer, Solved!";
+  } else {
+    solutionDisplay.innerHTML = "This puzzle is unsolvable";
   }
 };
 
